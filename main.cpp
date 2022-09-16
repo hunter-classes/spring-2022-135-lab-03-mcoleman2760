28989c26ceb4a889e6c651fa6d3d289c681c7cdf
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2022-09-16 05:27:23 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include "reservoir.h"
 
 int main() {
-  std::cout << "Enter date: 01/04/2018\n";
-  std::string date = "05/20/2018";
+  std::cout << "Enter date: 04/30/2018\n";
+  std::string date = "04/30/2018";
   std::cout << "East basin storage: " << get_east_storage(date) << " billion gallons \n";
 }
