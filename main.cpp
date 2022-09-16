db89a89ad7536a2fbe5effa7ff547de1fdf642bf
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2022-09-16 05:25:19 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,31 @@
-#include <iostream>
-#include <fstream>
-#include <cstdlib>
-#include <climits>
-#include <string.h>
 #include "reservoir.h"
+#include <climits>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string.h>
 
-int main() {
-  std::cout << "Enter date: 01/04/2018\n";
-  std::string date = "05/20/2018";
-  std::cout << "East basin storage: " << get_east_storage(date) << " billion gallons \n";
+double get_east_storage(std::string date) {
+
+  std::string Point_time;
+  double AUGEVolume;
+
+  std::ifstream fin("Current_Reservoir_Levels.tsv");
+  if (fin.fail()) {
+    std::cerr << "File cannot be opened for reading." << std::endl;
+    exit(1); // exit if failed to open the file
+  }
+  std::string junk;   // new string variable
+  getline(fin, junk); // read one line from the file
+
+ while(fin >> Point_time >> AUGEVolume) {
+    fin.ignore(INT_MAX, '\n'); //skips to the end of line,
+            //ignorring the remaining columns
+
+    if(date == Point_time) {
+        return AUGEVolume;
 }
+   }
+fin.close();
+return 0;
+  }
